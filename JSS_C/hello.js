f56b07feb5d72009cfba7c2232eb4a98bfb0f27a
PRIYANSHU KUMAR SINGH <160528175+PriyanshuSingh21july@users.noreply.github.com>
2024-02-23 00:02:05 +0530

    Add files via upload

---
+++ JSS_C/hello.js
@@ -0,0 +1,13 @@
+console.log("Priyanshu");
+console.log("Priya");
+
+
+document.write("I priyanshu<br>");
+document.write("Priyanshu is developer<br>");
+
+ setTimeout(()=>
+ {
+                document.write("I am student")
+ },200);
+
+ document.write("Priyanshu is Fullstack developer");
